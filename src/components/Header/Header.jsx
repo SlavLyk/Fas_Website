250d1e70fa43a33d0c7715d02abee2b765e8b876
Admin <admin@Air-Admin.cg.shawcable.net>
2022-06-20 16:13:12 -0600

    June20

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,36 @@
-import { Link } from 'react-router-dom'
-import './Header.css'   
-import Logo from '../../icons/Logo.svg'
-// import BurgerMenu from '../../icons/BurgerMenu.svg'
+import { Link } from "react-router-dom";
+import "./Header.css";
+import Logo from "../../icons/Logo.svg";
+import BurgerMenu from "../../icons/BurgerMenu.svg";
 
 function Header() {
-    return (    
-        <div className ='header'>
-             <Link to='/' className="logo">
-                <img height={40} src={Logo} alt='Logo'/>
-            </Link>
-            <div className ='headerMid'>
-                <div className ='headerItem'>
-                    <Link to='/games'>
-                        Games
-                    </Link>
-                </div>
-                <div className ='headerItem'>
-                    <Link to='/about'>
-                        About
-                    </Link>
-                </div>
-                <div className ='headerItem'>
-                    <Link to='/contact'>
-                        Contact
-                    </Link>
-                </div>  
-            </div>
-            <div className = 'burgerMenu'>
-                {/* <img height={20} width={34} src={BurgerMenu} alt='Logo'/> */}
-            </div>
+  return (
+    <div className="header">
+      <Link to="/" className="logo">
+        <img height={40} src={Logo} alt="Logo" />
+      </Link>
+      <div className="headerMid">
+        <div className="headerItem">
+          <Link to="/games">Games</Link>
         </div>
-    )
+        <div className="headerItem">
+          <Link to="/about">About</Link>
+        </div>
+        <div className="headerItem">
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+      <div className="burgerMenu">
+        <img
+          className="burgerIcon"
+          height={20}
+          width={34}
+          src={BurgerMenu}
+          alt="Logo"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
