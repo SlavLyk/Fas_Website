5b4d18e9a652f957223eaa5ecae462b6fc864824
SlavLyk <svyatlykhosherstov@gmail.com>
2022-09-10 15:35:47 -0600

    mailchimp api back online3

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "production") {
   // Serve any static files
   app.use(express.static(path.join(__dirname, "client/build")));
   // Handle React routing, return all requests to React app
-  app.get("/", function (_, res) {
+  app.get("/*", function (_, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
